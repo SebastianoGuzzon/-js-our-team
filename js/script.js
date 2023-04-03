720fd0d5ec37c94cab8ae8db6eafa66201dc1f66
SebastianoGuzzon <sebyguzz@icloud.com>
2023-04-03 19:21:30 +0200

    First Commit Information js

--- js/script.js
+++ js/script.js
@@ -1,7 +1,32 @@
-const gruppo[
-  {}
-  {}
-  {}
-  {}
-  {}
-]
+const gruppo = [
+{ 
+nome : 'Alessandro',
+cognome : 'Bastoni',
+ruolo : 'CEO',
+foto : '',
+},
+{
+nome : '',
+cognome : '',
+ruolo : '',
+foto : '',
+},
+{
+  nome : '',
+cognome : '',
+ruolo : '',
+foto : '',
+},
+{
+nome : '',
+cognome : '',
+ruolo : '',
+foto : '',
+},
+{
+nome : '',
+cognome : '',
+ruolo : '',
+foto : '',
+},
+];
